843c9125dc8fc3830db2afc717f7af047918179b
timeToCode-siri <sirilalitha9088@gmail.com>
2026-08-16 22:56:14 +0530

    fix(terraform): align flow logs with VPC reference implementation

--- terraform/flow_logs.tf
+++ terraform/flow_logs.tf
@@ -1,23 +1,44 @@
-resource "aws_flow_log" "app" {
-  iam_role_arn    = aws_iam_role.flow_logs.arn
-  log_destination = aws_cloudwatch_log_group.vpc_flow.arn
-  traffic_type    = "ALL"
-  vpc_id          = aws_vpc.app.id
-}
-
 resource "aws_cloudwatch_log_group" "vpc_flow" {
-  name              = "/hybrid-security/vpc-flow"
+  name              = "/cisco/security/vpc-flow-logs"
   retention_in_days = 30
 }
 
 resource "aws_iam_role" "flow_logs" {
-  name = "hybrid-security-vpc-flow-logs"
+  name = "CiscoVpcFlowLogsRole"
+
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
       Effect = "Allow"
-      Principal = { Service = "vpc-flow-logs.amazonaws.com" }
+      Principal = {
+        Service = "vpc-flow-logs.amazonaws.com"
+      }
       Action = "sts:AssumeRole"
     }]
   })
+}
+
+resource "aws_iam_role_policy" "flow_logs" {
+  role = aws_iam_role.flow_logs.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ]
+      Resource = "*"
+    }]
+  })
+}
+
+resource "aws_flow_log" "app_a" {
+  vpc_id               = aws_vpc.this["app_a"].id
+  traffic_type         = "ALL"
+  iam_role_arn         = aws_iam_role.flow_logs.arn
+  log_destination_type = "cloud-watch-logs"
+  log_destination      = aws_cloudwatch_log_group.vpc_flow.arn
 }
